c455c9207b77c3daf6028f23b714b8841f7e5358
Jackson F. de A. Mafra <jacksonfdam@gmail.com>
2016-11-05 15:01:18 -0200

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var app = express();
 
-app.use(express.static(__dirname + '/Exercicios'));
+app.use(express.static(__dirname + '/exercicios'));
 
 app.listen(8000);
